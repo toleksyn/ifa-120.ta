a7daac18909bf0511ad0d136f194a5fc4ce8a68a
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-26 14:22:23 +0200

    Reformat Code

--- src/main/java/com/softserveinc/ita/vpetrat/pageobjects/GoogleSearchImageResultsPage.java
+++ src/main/java/com/softserveinc/ita/vpetrat/pageobjects/GoogleSearchImageResultsPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class GoogleSearchImageResultsPage {
     public GoogleHomePage returnToHomePage() {
         TestRunner
