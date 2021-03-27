7870c208761d64af967921bf844627e057ec6e66
gura3 <gura3a@gmail.com>
2021-03-27 11:52:20 +0200

    little fixes in GoogleTest

--- src/test/java/com/softserveinc/ita/pageobjects_task/gura/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/pageobjects_task/gura/GoogleTest.java
@@ -2,8 +2,11 @@ package com.softserveinc.ita.pageobjects_task.gura;
 
 import com.softserveinc.ita.gura.GoogleHomePage;
 import com.softserveinc.ita.gura.GoogleResultPage;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,32 +14,36 @@ public class GoogleTest {
     private GoogleHomePage googlePage = new GoogleHomePage();
     private GoogleResultPage resultPage = new GoogleResultPage();
 
-    public void testingContainOfLink(int indexOfLink, String searchText, String textInLink) {
+    @Before
+    public void testingContainOfLink() {
         System.setProperty("webdriver.chrome.driver", ".C:\\chromedriver.exe");
         googlePage.openPage();
-        googlePage.doSearch(searchText);
-        assert(checkLinkHasText(indexOfLink, textInLink));
     }
 
-    public boolean checkLinkHasText(int indexOfLink, String link) {
-        if (link.contains(
-                Stream.of(GoogleResultPage.getDriver()
-                        .findElements(By.xpath("//h3[@class='LC20lb DKV0Md']")))
-                        .map(element -> element
-                                .get(indexOfLink)
-                                .getText())
-                        .collect(Collectors
-                                .joining()))) return true;
+    public String extractTextFromLink(int indexOfLink) {
+         return Stream.of(GoogleResultPage.getDriver()
+                    .findElements(By.xpath("//h3[@class='LC20lb DKV0Md']")))
+                    .map(element -> element
+                            .get(indexOfLink)
+                            .getText())
+                    .map(String::toString)
+                    .collect(Collectors.joining());
+    }
+
+    public boolean linkHasText(String link, String contains) {
+        if (link.contains(contains)) return true;
         else return false;
     }
 
     @Test
     public void testingSearchWithSearchTextFunnyKitten() {
-        testingContainOfLink(0, "funny kitten", "funnykitten");
+        googlePage.doSearch("funny kittens");
+        assert(linkHasText(extractTextFromLink(0), "funnykitten"));
     }
 
     @Test
     public void testingSearchWithSearchTextSmartphone() {
-        testingContainOfLink(0, "smartphone", "wikipedia");
+        googlePage.doSearch("smartphone");
+        assert(linkHasText(extractTextFromLink(0), "wikipedia"));
     }
 }
