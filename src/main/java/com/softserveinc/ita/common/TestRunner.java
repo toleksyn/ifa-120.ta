47a1efb5d3baf3418ac510dcb58680d48609e668
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-29 19:47:30 +0300

    Fixed TestRunner AfterClass annotation

--- src/main/java/com/softserveinc/ita/common/TestRunner.java
+++ src/main/java/com/softserveinc/ita/common/TestRunner.java
@@ -3,6 +3,7 @@ package com.softserveinc.ita.common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 
@@ -26,7 +27,7 @@ public class TestRunner {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @AfterMethod
+    @AfterClass
     public void close() {
         driver.close();
     }
