3f5b2420adac18ffd9501c9d76882930a5b15631
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-30 11:52:34 +0300

    Fixed merge issue

--- src/main/java/com/softserveinc/ita/vpetrat/GoogleHomePage.java
+++ src/main/java/com/softserveinc/ita/vpetrat/GoogleHomePage.java
@@ -1,6 +1,6 @@
-package com.softserveinc.ita.vpetrat.pageobjects;
+package com.softserveinc.ita.vpetrat;
 
-import com.softserveinc.ita.vpetrat.testrunner.TestRunner;
+import com.softserveinc.ita.common.TestRunner;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,11 +12,11 @@ public class GoogleHomePage {
         return this;
     }
 
-    public GoogleSearchResultPage searchFor(String request) {
+    public GoogleSearchResultsPage searchFor(String request) {
         TestRunner
                 .getWait()
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='gLFyf gsfi']")))
                 .sendKeys(request + Keys.ENTER);
-        return new GoogleSearchResultPage();
+        return new GoogleSearchResultsPage();
     }
 }
