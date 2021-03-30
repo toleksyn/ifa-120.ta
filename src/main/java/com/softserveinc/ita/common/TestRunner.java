6c7e0c558bd61ebe27e1b8c302ff43ac10d23a06
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-30 09:50:04 +0300

    Merge Issue

--- src/main/java/com/softserveinc/ita/common/TestRunner.java
+++ src/main/java/com/softserveinc/ita/common/TestRunner.java
@@ -3,7 +3,7 @@ package com.softserveinc.ita.common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeSuite;
 
 
@@ -27,7 +27,7 @@ public class TestRunner {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @AfterSuite
+    @AfterClass
     public void close() {
         driver.close();
     }
