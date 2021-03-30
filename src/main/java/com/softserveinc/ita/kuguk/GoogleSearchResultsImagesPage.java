d87ff38f5f5e7c8b83c8d21202879f4f0612ae05
kuguk <kuguk@ukr.net>
2021-03-30 20:03:25 +0300

    added "wait" into getSearchResultsImagesText()

--- src/main/java/com/softserveinc/ita/kuguk/GoogleSearchResultsImagesPage.java
+++ src/main/java/com/softserveinc/ita/kuguk/GoogleSearchResultsImagesPage.java
@@ -1,12 +1,16 @@
 package com.softserveinc.ita.kuguk;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.softserveinc.ita.common.TestRunner;
 
 public class GoogleSearchResultsImagesPage {
 
 	public String[] getSearchResultsImagesText() {		
+		TestRunner.getWait()
+		.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='VFACy kGQAp sMi44c lNHeqe WGvvNb']")));
+		
 		return TestRunner.getDriver().findElements(By.xpath("//*[@class='VFACy kGQAp sMi44c lNHeqe WGvvNb']"))
 				.stream()
 				.map(foundImagesList -> foundImagesList.getAttribute("title"))
