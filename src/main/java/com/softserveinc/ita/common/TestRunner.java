93f05436bc51e22c53a77d98ab2bb3a9e162827e
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-30 09:36:41 +0300

    Merged with development-branch

--- src/main/java/com/softserveinc/ita/common/TestRunner.java
+++ src/main/java/com/softserveinc/ita/common/TestRunner.java
@@ -3,7 +3,6 @@ package com.softserveinc.ita.common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
