dec2d2756b4dde7743fabe04f97143a3bcc0ac33
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-30 10:09:48 +0300

    Changed annotation

--- src/main/java/com/softserveinc/ita/common/TestRunner.java
+++ src/main/java/com/softserveinc/ita/common/TestRunner.java
@@ -3,7 +3,7 @@ package com.softserveinc.ita.common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 
@@ -27,7 +27,7 @@ public class TestRunner {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @AfterClass
+    @AfterSuite
     public void close() {
         driver.close();
     }
