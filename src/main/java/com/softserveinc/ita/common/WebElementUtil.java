7e5a7b37aca60b15b17b327fc59cf72dfc6dc20c
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-04-01 11:13:17 +0300

    Added WebElementUtil logic

--- src/main/java/com/softserveinc/ita/common/WebElementUtil.java
+++ src/main/java/com/softserveinc/ita/common/WebElementUtil.java
@@ -1,2 +1,25 @@
-package com.softserveinc.ita.common;public class WebElementUtil {
+package com.softserveinc.ita.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class WebElementUtil {
+    public static void clickElement(String webElementXPath) {
+        TestRunner.getWait().until(ExpectedConditions.elementToBeClickable(By.xpath(webElementXPath))).click();
+    }
+
+    public static void setValueForElement(String webElementXPath, String value) {
+        TestRunner.getWait().until(ExpectedConditions.elementToBeClickable(By.xpath(webElementXPath))).sendKeys(value);
+    }
+
+    public static List<WebElement> getListOfElements(String webElementsXPath) {
+        return TestRunner.getWait().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(webElementsXPath)));
+    }
+
+    public static WebElement getElementFromListForIndex(String webElementsXPath, int elementIndex) {
+        return getListOfElements(webElementsXPath).get(elementIndex);
+    }
 }
