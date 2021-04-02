57577d34ad10cbdc24d1891e06f506d21d39880f
Viva Natalya <vivanatalya@gmail.com>
2021-04-02 07:24:55 +0300

    refactored tests logic using WebElementUtil

--- src/main/java/com/softserveinc/ita/nromanchuk/GoogleSearchResultPage.java
+++ src/main/java/com/softserveinc/ita/nromanchuk/GoogleSearchResultPage.java
@@ -1,7 +1,6 @@
 package com.softserveinc.ita.nromanchuk;
 
-import com.softserveinc.ita.common.TestRunner;
-import org.openqa.selenium.By;
+import com.softserveinc.ita.common.WebElementUtil;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,29 +8,27 @@ import java.util.stream.Collectors;
 public class GoogleSearchResultPage {
 
     public String getLinkText(int index) {
-        return TestRunner.getDriver().findElements(By.xpath("//h3[@class='LC20lb DKV0Md']"))
-                .stream()
-                .map(element -> element.getText().toLowerCase())
-                .collect(Collectors.toList()).get(index);
+        return WebElementUtil.getElementFromListByIndex("//h3[@class='LC20lb DKV0Md']", index)
+                .getText()
+                .toLowerCase();
     }
 
     public List<String> getResultsLinks() {
-        return TestRunner
-                .getDriver()
-                .findElements(By.xpath("//div[@class='yuRUbf']/a"))
+        return WebElementUtil.getListOfElements("//div[@class='yuRUbf']/a")
                 .stream()
                 .map(webElement -> webElement.getAttribute("href"))
                 .collect(Collectors.toList());
     }
 
     public ImagesResultsPage navigateToImageResultsPage() {
-        TestRunner.getDriver().findElement(By.xpath("//*[@data-hveid='CAEQAw']")).click();
+        WebElementUtil.clickElement("//*[@data-hveid='CAEQAw']");
         return new ImagesResultsPage();
     }
 
     public BooksResultsPage navigateToBooksResultsPage() {
-        TestRunner.getDriver().findElement(By.xpath("//div[@class='GOE98c']")).click();
-        TestRunner.getDriver().findElements(By.xpath("//div[@class='gTMtLb fp-nh']//a")).get(1).click();
+        WebElementUtil.clickElement("//div[@class='GOE98c']");
+        WebElementUtil.getElementFromListByIndex("//div[@class='gTMtLb fp-nh']//a", 1)
+                .click();
         return new BooksResultsPage();
     }
 }
