1618877101ec6f969fef38de1bb50268a0f1f8fd
kuguk <kuguk@ukr.net>
2021-04-08 14:37:58 +0300

    small changes in code GoogleSettingsPage

--- src/main/java/com/softserveinc/ita/kuguk/GoogleSettingsPage.java
+++ src/main/java/com/softserveinc/ita/kuguk/GoogleSettingsPage.java
@@ -7,7 +7,7 @@ import com.codeborne.selenide.Selenide;
 public class GoogleSettingsPage {
 	public GoogleHomePage changeGoogleLanguage(String languageMark) {
 		$x("//a [@href='#languages']").click();;
-		$x("//div [@data-value='" + languageMark + "']").click();
+		$x(String.format("//div [@data-value='%s']", languageMark)).click();
 		$x("//div [@class='goog-inline-block jfk-button jfk-button-action']").click();
 		Selenide.confirm();
 		return new GoogleHomePage();
