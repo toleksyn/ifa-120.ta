d2e72e340c8a517dcbc4b19785619097b0b5e87e
SofiiaPro <54141095+SofiiaPro@users.noreply.github.com>
2021-04-12 16:37:45 +0300

    create first rozetkaTest

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/RozetkaHeaderPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/RozetkaHeaderPage.java
@@ -1,5 +1,7 @@
 package com.softserveinc.ita.rozetka.page_objects;
 
+import com.codeborne.selenide.Selenide;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -8,7 +10,6 @@ public class RozetkaHeaderPage {
         open("https://rozetka.com.ua/");
         return this;
     }
-
     public RozetkaSearchResultsPage searchFor(String request) {
         $x("//input[@class='search-form__input ng-untouched ng-pristine ng-valid']").val(request);
         $x("//button[@class='button button_color_green button_size_medium search-form__submit']").click();
---
+++ src/test/java/RozetkaTest.java
@@ -0,0 +1,30 @@
+import com.softserveinc.ita.common.TestRunner;
+import com.softserveinc.ita.rozetka.page_objects.RozetkaCategoryPage;
+import com.softserveinc.ita.rozetka.page_objects.RozetkaHomePage;
+import com.softserveinc.ita.rozetka.page_objects.RozetkaProductPage;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class RozetkaTest extends TestRunner {
+    private RozetkaHomePage rozetkaHomePage;
+
+    @Test
+    public void testSelectProductByCataloge() {
+        WebElement pageTitle = $x("//h1[@class='portal__heading']");
+        WebElement productTitle = $x("//h1[@class='product__title']");
+
+        rozetkaHomePage = new RozetkaHomePage().openHomePage();
+        RozetkaCategoryPage catalogeCategoryItem = rozetkaHomePage
+                .navigateToCategoryPage(0);
+        Assert.assertTrue(pageTitle.getText().contains("Компьютеры"));
+        RozetkaProductPage chosenProduct = catalogeCategoryItem
+                .navigateToProductCategoryPage(1)
+                .navigateToProductByNumber(0);
+        Assert.assertTrue(productTitle.getText().contains("Компьютер"));
+        chosenProduct.returnToCategoryPage(1);
+        Assert.assertTrue(pageTitle.getText().contains("Компьютеры"));
+    }
+}
