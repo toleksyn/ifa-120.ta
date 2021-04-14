803e2a55dfd499d3c1b3ca27f69f820d4ab2f96e
SerhiiHladkyi <gladkiy.sergiy18@gmail.com>
2021-04-14 20:24:42 +0300

    shladkyi - add test to cover adding product to basket

--- src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
@@ -1,8 +1,12 @@
 package com.softserveinc.ita.rozetka_test;
 
 import com.softserveinc.ita.common.TestRunner;
+import com.softserveinc.ita.rozetka.page_objects.BasketPage;
 import com.softserveinc.ita.rozetka.page_objects.HomePage;
+import com.softserveinc.ita.rozetka.page_objects.ProductPage;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class RozetkaProductNavigationTest extends TestRunner {
 
@@ -11,6 +15,18 @@ public class RozetkaProductNavigationTest extends TestRunner {
     @BeforeMethod
     public void openHomepage() {
         rozetkaHomePage = new HomePage().openHomePage();
+    }
+
+    @Test
+    public void testAddingProductToBasket() {
+        ProductPage productPage = rozetkaHomePage
+                .getLeftSidebar()
+                .openCategory("Сантехніка")
+                .openProductsListPage("Ванни")
+                .openProductByNumber(1);
+        BasketPage basketPage = productPage.addProductToBasket();
+        Assert.assertEquals(basketPage.getProductTitleByName("Ванна"), productPage.getProductTitle(),
+                "Product should be added to basket");
     }
 }
 
