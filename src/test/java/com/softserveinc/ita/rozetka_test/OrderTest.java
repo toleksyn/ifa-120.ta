07b32a2e4edda762445931e7004d407a9ce2d502
kuguk <kuguk@ukr.net>
2021-04-15 00:27:28 +0300

    added testEditOrder()

--- src/test/java/com/softserveinc/ita/rozetka_test/OrderTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/OrderTest.java
@@ -4,7 +4,9 @@ import com.softserveinc.ita.common.TestRunner;
 import com.softserveinc.ita.rozetka.page_objects.BasketPage;
 import com.softserveinc.ita.rozetka.page_objects.HomePage;
 import com.softserveinc.ita.rozetka.page_objects.OrderPage;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class OrderTest extends TestRunner {
 
@@ -19,5 +21,35 @@ public class OrderTest extends TestRunner {
                 .openProductByNumber(1)
                 .addProductToBasket()
                 .openOrderPage();
+    }
+
+    @Test
+    public void testEditOrder() {
+        int orderProductCount;
+        int orderSum;
+        int changedOrderProductCount;
+        int changedOrderSum;
+
+        openOrderPage.startEditingProductInBasket();
+        openBasketPage = new BasketPage();
+        orderProductCount = openBasketPage.getProductCount();
+        orderSum = openBasketPage.getOrderProductPrice();
+        openBasketPage.increaseProductCount();
+        changedOrderProductCount = openBasketPage.getProductCount();
+        changedOrderSum = openBasketPage.getOrderProductPrice();
+
+        Assert.assertEquals(changedOrderProductCount, 2 * orderProductCount);
+        Assert.assertEquals(changedOrderSum, 2 * orderSum);
+
+        openBasketPage.openOrderPage();
+        openOrderPage.startEditingProductInBasket();
+        openBasketPage.decreaseProductCount();
+        changedOrderProductCount = openBasketPage.getProductCount();
+        changedOrderSum = openBasketPage.getOrderProductPrice();
+
+        Assert.assertEquals(changedOrderProductCount, orderProductCount);
+        Assert.assertEquals(changedOrderSum, orderSum);
+
+        openBasketPage.openOrderPage();
     }
 }
