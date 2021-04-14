c9d7341b5765040ed4f0db2c2532b4304b36e022
Roman Khrabatyn <romankhr@yahoo.com>
2021-04-14 21:20:40 +0300

    New input methods

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/OrderPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/OrderPage.java
@@ -3,11 +3,13 @@ package com.softserveinc.ita.rozetka.page_objects;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class OrderPage {
-    public OrderPage fillAllInputFields(){
-        $x("//div[@class='form__row js-surname']/input").setValue("Петренко");
-        $x("//div[@class='form__row js-name']/input").setValue("Вася");
-        $x("//div[@class='autocomplete']/input").setValue("Івано-Франківськ").click();
-        $x("//div[@class='form__row js-phone']/input").setValue("77777777");
+    public OrderPage setSurname(String surname) {
+        $x("//div[@class='form__row js-surname']/input").setValue(surname);
+        return this;
+    }
+
+    public OrderPage setName(String name) {
+        $x("//div[@class='form__row js-name']/input").setValue(name);
         return this;
     }
 
