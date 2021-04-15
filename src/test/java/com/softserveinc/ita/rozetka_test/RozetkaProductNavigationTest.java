c7e846e420e9c3dd59bef541ee277ea526bc37a0
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-04-15 19:26:47 +0300

    Fixed comments

--- src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
@@ -6,12 +6,13 @@ import org.testng.annotations.BeforeMethod;
 
 public class RozetkaProductNavigationTest extends TestRunner {
 
+    //This added for future tests
     private HomePage rozetkaHomePage;
 
+    //This added for future tests
     @BeforeMethod
     public void openHomepage() {
         rozetkaHomePage = new HomePage().openHomePage();
     }
-
 }
 
