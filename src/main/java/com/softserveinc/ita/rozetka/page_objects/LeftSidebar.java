ba50901430505c582aef26e8e14d0905eab50517
Viva Natalya <vivanatalya@gmail.com>
2021-04-15 13:42:46 +0300

    2

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/LeftSidebar.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/LeftSidebar.java
@@ -2,7 +2,7 @@ package com.softserveinc.ita.rozetka.page_objects;
 
 import static com.codeborne.selenide.Selenide.$x;
 
-public class LeftSidebar {
+public class LeftSidebar {git
 
     public CategoryPage openCategory(String categoryName){
         $x(String.format("//a[@class = 'menu-categories__link' and contains(text(), '%s')]", categoryName))
--- src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
@@ -2,10 +2,7 @@ package com.softserveinc.ita.rozetka_test;
 
 import com.softserveinc.ita.common.TestRunner;
 import com.softserveinc.ita.rozetka.page_objects.HomePage;
-import com.softserveinc.ita.rozetka.page_objects.ProductPage;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class RozetkaProductNavigationTest extends TestRunner {
 
@@ -13,29 +10,8 @@ public class RozetkaProductNavigationTest extends TestRunner {
 
     @BeforeMethod
     public void openHomepage() {
-        rozetkaHomePage = new HomePage().openHomePage();
-    }
-
-    @Test
-    public void testPossibilityViewingProductDescriptions() {
-        ProductPage productPage = rozetkaHomePage.getLeftSidebar
-                ("Ноутбуки")
-                .openProductsListPage("Планшети")
-                .openProductByNumber(1)
-                .openProductTabByName("Характеристики");
-        int characteristicsListSize = productPage
-                .getCharacteristicListSize(1);
-        Assert.assertTrue(characteristicsListSize > 0, "product characteristic description" +
-                " should contain at least 1 row");
-        String productTabsTitle = productPage
-                .openProductTabByName("Відгуки")
-                .getProductTabsTitle();
-        Assert.assertTrue(productTabsTitle.contains("Відгуки"), "product tabs title should contains 'Відгуки'");
-        int questionsRowsAmount = productPage
-                .openProductTabByName("Питання")
-                .getQuestionListSize(1);
-        Assert.assertTrue(questionsRowsAmount > 0, "list size  should contain at least 1 text field");
-
+        rozetkaHomePage = new HomePage()
+                .openHomePage();
     }
 
 }
