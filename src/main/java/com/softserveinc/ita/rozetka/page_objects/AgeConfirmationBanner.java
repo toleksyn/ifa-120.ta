aac2c3a36d2c1928ee3ec1ea7c952f054df8e062
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-04-19 12:57:57 +0300

    Fixed

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/AgeConfirmationBanner.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/AgeConfirmationBanner.java
@@ -3,6 +3,7 @@ package com.softserveinc.ita.rozetka.page_objects;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class AgeConfirmationBanner {
+
     public ProductsListPage acceptAdultAge() {
         $x("//a[class='btn-link-i exponea-close']").click();
         return new ProductsListPage();
