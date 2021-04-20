2c1219449a3bf6c3684893a28e50c9e01ff5ecc0
Viva Natalya <vivanatalya@gmail.com>
2021-04-20 22:33:37 +0300

    1

--- src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
@@ -31,35 +31,5 @@ public class RozetkaProductNavigationTest extends TestRunner {
         assertEquals(basketPage.getProductTitleByName("Ванна"), productTitle,
                 "Product should be added to basket");
     }
-
-    @Test
-    public void testSelectProductBySearch() {
-        var searchRequest = "гаманець";
-        var productTitle = rozetkaHomePage
-                .getHeader()
-                .searchFor(searchRequest)
-                .openProductByNumber(1)
-                .getProductTitle();
-        assertTrue(productTitle.contains(searchRequest),
-                "Product title should contain search request");
-    }
-
-    @Test
-    public void testResultsOnProductsListPage() {
-        var productCategoryName = "Ноутбуки";
-        var productListPage = rozetkaHomePage
-                .getLeftSidebar()
-                .openCategory("Ноутбуки та комп’ютери")
-                .openProductsListPage(productCategoryName);
-        assertEquals(productListPage.getPageTitle(), productCategoryName, format("Title should be '%s'", productCategoryName));
-        var expectedProductType = "Ноутбук";
-        assertTrue(productListPage.getProductName(1).contains(expectedProductType),
-                format("First product should be '%s'", expectedProductType));
-        var productCount = productListPage.getProductListSize();
-        assertTrue(productListPage.getProductName(productCount / 2).contains(expectedProductType),
-                format("Middle product should be '%s'", expectedProductType));
-        assertTrue(productListPage.getProductName(productCount).contains(expectedProductType),
-                format("Last product should be '%s'", expectedProductType));
-    }
 }
 
