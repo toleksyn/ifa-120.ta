147f301204653c2a150f50e970c632d63d4c5e25
SerhiiHladkyi <gladkiy.sergiy18@gmail.com>
2021-04-20 18:37:02 +0300

    shladkyi - add test to cover language changing

--- src/test/java/com/softserveinc/ita/rozetka_test/HamburgerTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/HamburgerTest.java
@@ -4,18 +4,27 @@ import com.softserveinc.ita.common.TestRunner;
 import com.softserveinc.ita.rozetka.page_objects.HamburgerPage;
 import com.softserveinc.ita.rozetka.page_objects.HomePage;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.title;
+import static org.testng.Assert.assertTrue;
 
 public class HamburgerTest extends TestRunner {
 
-    //This added for future tests
     private HamburgerPage hamburgerPage;
 
-    //This added for future tests
     @BeforeMethod
     public void openHamburgerPage() {
         hamburgerPage = new HomePage()
                 .openHomePage()
                 .getHeaderPage()
                 .openHamburgerPage();
+    }
+
+    @Test
+    public void testLanguageChanging() {
+        var homePage = hamburgerPage.changeLanguage();
+        assertTrue(title().contains("Интернет-магазин"), "Title should contains 'Интернет-магазин'");
+        assertTrue(homePage.getHeaderPage().isSearchButtonVisible(), "Search button should be visible");
     }
 }
