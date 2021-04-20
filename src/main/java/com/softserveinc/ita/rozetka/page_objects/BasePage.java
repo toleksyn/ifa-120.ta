337bb72b33537c005c98ab21fab39dc7b48f7402
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-04-20 11:10:51 +0300

    Added Lombok support

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/BasePage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/BasePage.java
@@ -1,15 +1,11 @@
 package com.softserveinc.ita.rozetka.page_objects;
 
+import lombok.Getter;
+
+@Getter
 public abstract class BasePage {
 
     private final Header headerPage = new Header();
     private final Footer footerPage = new Footer();
 
-    public Header getHeaderPage() {
-        return headerPage;
-    }
-
-    public Footer getFooterPage() {
-        return footerPage;
-    }
 }
