296806ac9b0445fd3e704730d0e33dee6dae431e
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-04-21 11:13:22 +0300

    Fixed comments

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductPage.java
@@ -15,7 +15,6 @@ public class ProductPage extends BasePage {
     }
 
     public String getViewedProductName(int number) {
-
         return $x(format("(//section[@class='recently-viewed']//a[@class='lite-tile__title'])[%d]", number)).text();
     }
 
