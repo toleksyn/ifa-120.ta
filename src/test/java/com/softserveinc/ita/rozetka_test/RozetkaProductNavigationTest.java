e19214caec912f88b91b4fad3c6c78e9e010d8db
Viva Natalya <vivanatalya@gmail.com>
2021-04-21 06:14:30 +0300

    1

--- src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/RozetkaProductNavigationTest.java
@@ -15,8 +15,7 @@ public class RozetkaProductNavigationTest extends TestRunner {
 
     @BeforeMethod
     public void openHomepage() {
-        rozetkaHomePage = new HomePage()
-                .openHomePage();
+        rozetkaHomePage = new HomePage().openHomePage();
     }
 
     @Test
@@ -30,6 +29,36 @@ public class RozetkaProductNavigationTest extends TestRunner {
         var basketPage = productPage.addProductToBasket();
         assertEquals(basketPage.getProductTitleByName("Ванна"), productTitle,
                 "Product should be added to basket");
+    }
+
+    @Test
+    public void testSelectProductBySearch() {
+        var searchRequest = "гаманець";
+        var productTitle = rozetkaHomePage
+                .getHeader()
+                .searchFor(searchRequest)
+                .openProductByNumber(1)
+                .getProductTitle();
+        assertTrue(productTitle.contains(searchRequest),
+                "Product title should contain search request");
+    }
+
+    @Test
+    public void testResultsOnProductsListPage() {
+        var productCategoryName = "Ноутбуки";
+        var productListPage = rozetkaHomePage
+                .getLeftSidebar()
+                .openCategory("Ноутбуки та комп’ютери")
+                .openProductsListPage(productCategoryName);
+        assertEquals(productListPage.getPageTitle(), productCategoryName, format("Title should be '%s'", productCategoryName));
+        var expectedProductType = "Ноутбук";
+        assertTrue(productListPage.getProductName(1).contains(expectedProductType),
+                format("First product should be '%s'", expectedProductType));
+        var productCount = productListPage.getProductListSize();
+        assertTrue(productListPage.getProductName(productCount / 2).contains(expectedProductType),
+                format("Middle product should be '%s'", expectedProductType));
+        assertTrue(productListPage.getProductName(productCount).contains(expectedProductType),
+                format("Last product should be '%s'", expectedProductType));
     }
 }
 
