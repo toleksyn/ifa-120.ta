aec18eb2268106e9939f6b139928621145374314
Viva Natalya <vivanatalya@gmail.com>
2021-04-25 18:50:20 +0300

    1

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductsListPageWithSubcategories.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductsListPageWithSubcategories.java
@@ -1,4 +1,5 @@
 package com.softserveinc.ita.rozetka.page_objects;
 
 public class ProductsListPageWithSubcategories extends ProductsListPage {
+    // here will be added methods for working with categories, when it'll be needed
 }
