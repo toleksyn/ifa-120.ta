c33eaece8497948097897fe81a5141d4f5d2e281
SerhiiHladkyi <gladkiy.sergiy18@gmail.com>
2021-05-10 14:42:01 +0300

    shladkyi - add test to cover filter products by price range

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductsListPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/ProductsListPage.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 import static java.lang.Integer.parseInt;
+import static java.lang.String.*;
 import static java.lang.String.format;
 
 public class ProductsListPage extends BasePage {
@@ -81,5 +82,20 @@ public class ProductsListPage extends BasePage {
     @Step("Products list page: get age confirmation popup")
     public AgeConfirmationPopup getAgeConfirmationPopup() {
         return new AgeConfirmationPopup();
+    }
+
+    public ProductsListPage setLowestPrice(int price) {
+        $x("//input[contains(@formcontrolname, 'min')]").val(valueOf(price));
+        return this;
+    }
+
+    public ProductsListPage setHighestPrice(int price) {
+        $x("//input[contains(@formcontrolname, 'max')]").val(valueOf(price));
+        return this;
+    }
+
+    public ProductsListPage confirmFilterPriceRange() {
+        $x("//button[contains(@class, 'slider-filter__button')]").click();
+        return this;
     }
 }
