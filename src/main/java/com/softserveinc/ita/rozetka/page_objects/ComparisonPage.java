1e036ae71d79c46b2f904f0666ade3b6b72a7512
Gura-Andriy <gura3a@gmail.com>
2021-05-13 01:27:46 +0300

    made test for see different in comparison

--- src/main/java/com/softserveinc/ita/rozetka/page_objects/ComparisonPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/page_objects/ComparisonPage.java
@@ -1,43 +1,26 @@
 package com.softserveinc.ita.rozetka.page_objects;
 
+import com.codepoetics.protonpack.Indexed;
+import com.codepoetics.protonpack.StreamUtils;
 import io.qameta.allure.Step;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.$$x;
 import static java.lang.String.format;
-import static java.util.stream.IntStream.range;
 
 public class ComparisonPage extends BasePage {
-
-    public List<String> getCharacteristicForProduct(int number) {
-        var allCharacteristics = $$x("//dd[@class='comparison-characteristic__value']")
-                .shouldHave(sizeGreaterThanOrEqual(1))
-                .texts();
-        var firstProductCharacteristics = new ArrayList<String>();
-        var secondProductCharacteristics = new ArrayList<String>();
-        range(0, allCharacteristics.size())
-                .forEach(index -> (index % 2 == 0 ? firstProductCharacteristics : secondProductCharacteristics)
-                        .add(allCharacteristics.get(index)));
-        switch (number) {
-            case 1:
-                return firstProductCharacteristics;
-            case 2:
-                return secondProductCharacteristics;
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
     @Step("Comparison page: show differences")
     public ComparisonPage showDifferences() {
         $x("//button[contains(@class, 'comparison-settings__toggle')]").click();
         return this;
     }
 
+    @Step("Comparison page: delete product")
     public ComparisonPage deleteProduct(int productNumber) {
         $x(format("(//button[@class='button button--white button--small context-menu__toggle'])[%d]", productNumber)).click();
         $x("//button[@class='button button--medium button--with-icon button--link context-menu-actions__button']").click();
@@ -46,5 +29,22 @@ public class ComparisonPage extends BasePage {
 
     public String getAlertMessage() {
         return $x("//div[@class='form__hint form__hint_type_attention ng-star-inserted']").text();
+    }
+
+    @Step("Comparison page: get product characteristic")
+    public List<Indexed<String>> getProductCharacteristic(int productNumber) {
+        List<String> characteristicList = new ArrayList<>($$x(format("(//dt[contains(@class, 'comparison-characteristic__label')])[%d]", productNumber))
+                .texts());
+        if (productNumber == 1) {
+            return StreamUtils
+                    .zipWithIndex(characteristicList.stream())
+                    .filter(i -> (i.getIndex() + 1) % 2 != 0)
+                    .collect(Collectors.toList());
+        } else {
+            return StreamUtils
+                    .zipWithIndex(characteristicList.stream())
+                    .filter(i -> (i.getIndex() + 1) % productNumber == 0)
+                    .collect(Collectors.toList());
+        }
     }
 }
--- src/test/java/com/softserveinc/ita/rozetka_test/ComparisonTest.java
+++ src/test/java/com/softserveinc/ita/rozetka_test/ComparisonTest.java
@@ -1,13 +1,14 @@
 package com.softserveinc.ita.rozetka_test;
 
+import com.codepoetics.protonpack.Indexed;
 import com.softserveinc.ita.common.TestRunner;
 import com.softserveinc.ita.rozetka.page_objects.ComparisonPage;
 import com.softserveinc.ita.rozetka.page_objects.HomePage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static java.util.stream.IntStream.range;
-import static org.testng.Assert.assertTrue;
+import java.util.List;
+
 import static org.testng.AssertJUnit.assertEquals;
 
 public class ComparisonTest extends TestRunner {
@@ -28,33 +29,19 @@ public class ComparisonTest extends TestRunner {
     }
 
     @Test
-    public void testComparisonSettings() {
-        var firstProductCharacteristics = comparisonPage.getCharacteristicForProduct(1);
-        var secondProductCharacteristics = comparisonPage.getCharacteristicForProduct(2);
-
-        var allCharacteristicsSize = firstProductCharacteristics.size() + secondProductCharacteristics.size();
-        var isCharacteristicHaveSameItem = range(0, allCharacteristicsSize)
-                .anyMatch(index -> firstProductCharacteristics
-                        .get(index)
-                        .equals(secondProductCharacteristics.get(index)));
-        assertTrue(isCharacteristicHaveSameItem, "Characteristics of two products should have same item");
-
-        comparisonPage.showDifferences();
-        var differentFirstProductCharacteristics = comparisonPage.getCharacteristicForProduct(1);
-        var differentSecondProductCharacteristics = comparisonPage.getCharacteristicForProduct(2);
-
-        var isAllCharacteristicsDifferent = range(0, differentFirstProductCharacteristics.size())
-                .noneMatch(index -> differentFirstProductCharacteristics
-                        .get(index)
-                        .equals(differentSecondProductCharacteristics.get(index)));
-        assertTrue(isAllCharacteristicsDifferent, "Characteristics of two products should be different");
-    }
-
-    @Test
     public void testDeleteProductFromComparison() {
         comparisonPage
                 .deleteProduct(1);
         assertEquals(comparisonPage.getAlertMessage(),
                 "Недостатньо товарів для порівняння");
+    }
+
+    @Test
+    public void testShowOnlyDifferenceCharacteristic() {
+        comparisonPage.showDifferences();
+        List<Indexed<String>> firstProductCharacteristic = comparisonPage
+                .getProductCharacteristic(1);
+        List<Indexed<String>> secondProductCharacteristic = comparisonPage
+                .getProductCharacteristic(2);
     }
 }
